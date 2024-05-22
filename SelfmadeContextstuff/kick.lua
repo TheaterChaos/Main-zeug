d8e6b4f244eb7dc9fe894d719b36d3791b3c7abb
TheaterChaos <123401605+TheaterChaos@users.noreply.github.com>
2024-05-22 17:23:07 +0200

    Delete SelfmadeContextstuff directory

--- SelfmadeContextstuff/kick.lua
+++
@@ -1,8 +0,0 @@
-return {
-    name="Kick Player",
-    help="kicks this Player",
-    applicable_to={"PLAYER"},
-    execute=function(target)
-        menu.trigger_commands("kick"..target.name)
-    end
-}
