defb92ff688aceb1e766c688ef8aa53445c57c5b
TheaterChaos <123401605+TheaterChaos@users.noreply.github.com>
2024-05-31 15:59:51 +0200

    Delete lib/SelfmadeContextstuff directory

--- lib/SelfmadeContextstuff/destroy.lua
+++
@@ -1,9 +0,0 @@
-return {
-    name="Destroy",
-    help="Explode the vehicle",
-    applicable_to={"VEHICLE"},
-    execute=function(target)
-        local car = GET_ENTITY_COORDS(target.handle)
-        ADD_EXPLOSION(car.x, car.y, car.z, 7, 5000, false, true, 0.0, false)
-    end
-}
